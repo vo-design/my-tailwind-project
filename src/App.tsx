eb88da3ca956ef095c12f50af376d4362e14fcb0
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 12:13:12 +0300

    @utility Directive

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 const App = () => {
     return (
-        <div className='bg-deepblue p-12'>
+        <div className='bg-deepblue container'>
             <h1 className="text-5xl font-bold text-white">This is Tailwind CSS v4</h1>
             <button className="bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4">Click here</button>
         </div>
