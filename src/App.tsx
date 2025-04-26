3e5770cbc84989bf61f815ac79b4b97eb7d7b6a0
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 11:58:55 +0300

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <div className='bg-red-500 p-12'>
+            <h1 className="text-5xl font-bold text-white">This is Tailwind CSS v4</h1>
+        </div>
+    )
+}
+export default App
