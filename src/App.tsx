2547cf0cb97d72be74f745b1bd46a4e435610943
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 12:07:16 +0300

    @theme Directive

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
 const App = () => {
     return (
-        <div className='bg-red-500 p-12'>
+        <div className='bg-deepblue p-12'>
             <h1 className="text-5xl font-bold text-white">This is Tailwind CSS v4</h1>
+            <button className="bg-amber text-alabaster text-2xl font-bold mt-12 rounded px-6 py-4">Click here</button>
         </div>
     )
 }
