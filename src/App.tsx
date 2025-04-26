692c5022b1d781c0a3fce163b92eebbc435d6825
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 12:44:43 +0300

    3D Transform APIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 const App = () => {
     return (
-        <div className="@container border p-4">
-            <div className="flex gap-4">
-                <div className="bg-blue-500 h-40 w-64"></div>
-                <div className="bg-green-500 h-40 w-64 @min-md:@max-xl:hidden"></div>
-                <div className="bg-red-500 h-40 w-64"></div>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100 perpspective-1000">
+            <div
+                className="bg-blue-500 h-96 w-96 flex items-center justify-center text-white text-4xl font-bold transform-style-3d transform hover:scale-150 hover:translate-z-20 transition-transform duration-300">3D
+                Scale
             </div>
         </div>
     )
