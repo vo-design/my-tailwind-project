2ea6acfd2a251e44bef90d09c9e027355a9656de
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 12:34:11 +0300

    Container Queries

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 const App = () => {
     return (
-        <div className='bg-deepblue container'>
-            <h1 className="text-5xl font-bold text-white">This is Tailwind CSS v4</h1>
-            <button className="bg-amber text-green-500 border-4 text-2xl font-bold mt-12 rounded px-6 py-4 animate-pulse">Click here</button>
-            <input type="text" placeholder="Search for..." className="bg-white text-lg text-amber border-2 p-4 rounded ml-4 outline-0"/>
+        <div className="@container border p-4">
+            <div className="flex gap-4">
+                <div className="bg-blue-500 h-40 w-64"></div>
+                <div className="bg-green-500 h-40 w-64 @min-md:@max-xl:hidden"></div>
+                <div className="bg-red-500 h-40 w-64"></div>
+            </div>
         </div>
     )
 }
