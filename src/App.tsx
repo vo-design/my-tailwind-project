1f3fba5669e3d81f06a09125023f6cb8c217f4da
Volodymyr Onishchenko <hello@vdigital.design>
2025-04-26 12:24:00 +0300

    @plugin Directive

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ const App = () => {
     return (
         <div className='bg-deepblue container'>
             <h1 className="text-5xl font-bold text-white">This is Tailwind CSS v4</h1>
-            <button className="bg-amber text-green-500 border-4 text-2xl font-bold mt-12 rounded px-6 py-4">Click here</button>
+            <button className="bg-amber text-green-500 border-4 text-2xl font-bold mt-12 rounded px-6 py-4 animate-pulse">Click here</button>
             <input type="text" placeholder="Search for..." className="bg-white text-lg text-amber border-2 p-4 rounded ml-4 outline-0"/>
         </div>
     )
